957a8555fbc5105eef885b2939fd60eb3c74a7d6
Gideon Appau <gideonad.codez@gmail.com>
2023-06-23 16:01:17 +0000

    init commit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 let input = document.querySelector("input");
 const inputContainer = document.querySelector(".input-container");
 let icon = document.querySelector(".icon");
-icon.addEventListener("click", (e)=>{
-    e.preventDefault()
-    transferData();
-})
+icon.addEventListener("click", (e) => {
+  e.preventDefault();
+  transferData();
+});
 
-inputContainer.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    transferData();
-})
+inputContainer.addEventListener("submit", (e) => {
+  e.preventDefault();
+  transferData();
+});
 function transferData() {
-    // Encode the data to be passed in the URL
-    var encodedData = encodeURIComponent(input.value);
-    // Construct the URL with the encoded data
-    var url = "form.html?data=" + encodedData;
-    // Redirect to the destination page
-    window.location.href = url;
+  // Encode the data to be passed in the URL
+  var encodedData = encodeURIComponent(input.value);
+  // Construct the URL with the encoded data
+  var url = "form.html?data=" + encodedData;
+  // Redirect to the destination page
+  window.location.href = url;
 }
